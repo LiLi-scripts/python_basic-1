f1cf7a03e75454d666f447a8b1a37dfce6a8478f
swaldom <svichkar.dev@gmail.com>
2020-11-10 16:40:18 +0200

    upd hw2 (solutions)

--- hw2/numbers.py
+++ hw2/numbers.py
@@ -4,4 +4,38 @@
     2. Вывести, в каком порядке расположены цифры (возрастания/убывания/в разброс)
 """
 
+# Ниже описан один из вариантов решения задачи.
+
 number = int(input())
+
+if 1 <= number < 10:  # number >= 1 and number < 10
+    print('sum =', number)
+elif 10 <= number < 100:
+    digit_1 = number // 10
+    digit_2 = number % 10
+
+    print('sum =', digit_1 + digit_2)
+
+    if digit_1 < digit_2:
+        print('increase')
+    elif digit_1 > digit_2:
+        print('decrease')
+    else:
+        print('equal')
+elif 100 <= number < 1000:
+    digit_1 = number // 100
+    digit_2 = number // 10 % 10
+    digit_3 = number % 10
+
+    print('sum =', digit_1 + digit_2 + digit_3)
+
+    if digit_1 < digit_2 < digit_3:
+        print('increase')
+    elif digit_1 > digit_2 > digit_3:
+        print('decrease')
+    elif digit_1 == digit_2 == digit_3:
+        print('equal')
+    else:
+        print('random')
+else:
+    print('number must be from 1 to 999')
--- hw2/operators.py
+++ hw2/operators.py
@@ -14,6 +14,54 @@
     4. 25
 """
 
+# Ниже описан один из вариантов решения задачи.
+
 a = int(input('a: '))
 b = int(input('b: '))
 c = int(input('c: '))
+
+# 1.
+summ = 0
+
+if a % 5 == 0:
+    summ += a
+
+if b % 5 == 0:
+    summ += b
+
+if c % 5 == 0:
+    summ += c
+
+print('1.', summ)
+
+# 2 and 3.
+positive = 0
+negavite = 0
+
+if a > 0:
+    positive += 1
+elif a < 0:
+    negavite += 1
+
+if b > 0:
+    positive += 1
+elif b < 0:
+    negavite += 1
+
+if c > 0:
+    positive += 1
+elif c < 0:
+    negavite += 1
+
+print('2.', positive)
+print('3.', negavite)
+
+# 4.
+if a >= b >= c or b >= a >= c:
+    result = a + b
+elif a >= c >= b or c >= a >= b:
+    result = a + c
+else:
+    result = b + c
+
+print('4.', result)
