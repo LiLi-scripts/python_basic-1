10b77b86755b8988acf3f472310b4a3083fee63f
swaldom <svichkar.dev@gmail.com>
2020-11-14 18:33:49 +0200

    upd hw4 (solutions)

--- hw4/longest_word.py
+++ hw4/longest_word.py
@@ -4,3 +4,32 @@
     1. Вывести количество слов в введенной строке.
     2. Вывести самое длинное слово и его длину.
 """
+
+string = input('Enter a string: ') + ' '
+
+word_counter = 0
+longest_word = ''
+word = ''
+
+for i in string:
+    if i.isalpha():
+        word += i
+    else:
+        if word and len(longest_word) < len(word):
+            word_counter += 1
+            longest_word = word
+        word = ''
+
+print(word_counter, longest_word, len(longest_word))
+
+
+# Решение с использованием библиотеки re и функции max
+
+import re
+
+string = input('Enter a string: ')
+
+words = re.findall(r'[a-zA-Z]+', string)
+longest_word = max(words, key=len)
+
+print(len(words), longest_word, len(longest_word))
--- hw4/phone_format.py
+++ hw4/phone_format.py
@@ -6,3 +6,21 @@
     Если цифр в номере недостаточно, чтобы описать номер в нужном формате -
         попросить пользователя повторить ввод.
 """
+
+import re
+
+input_number = input('Enter phone number: ')
+
+number = ''
+for digit in input_number:
+    if digit.isdigit():
+        number += digit
+
+number = '380' + number[-9:]
+if len(number) != 12:
+    print('Wrong phone number. Please re-enter.')
+else:
+    print(number)
+
+# P.S. строки 14-17 можно заменить 1 строкой с использованием re.sub
+# number = re.sub(r'\D', '', input_number)
